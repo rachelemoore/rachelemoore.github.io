8b7b19e253b4d4783b61d1ae63bacdaa37330e1f
rachelemoore <124698533+rachelemoore@users.noreply.github.com>
2026-03-15 22:55:48 +0000

    Deploying to gh-pages from @ rachelemoore/rachelemoore.github.io@3971f1155ff179099b49fa5b3ba005eaab808f0b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -508,14 +508,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%72%61%63%68%65%6C.%6D%6F%6F%72%65@%6B%63%6C.%61%63.%75%6B", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-instagram',
+        title: 'Instagram',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://instagram.com/rach_e_moore", "_blank");
+        },
+      },{
+        id: 'social-orcid',
+        title: 'ORCID',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://orcid.org/0000-0002-5426-9545", "_blank");
         },
       },{
         id: 'social-rss',
@@ -529,14 +536,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=xaJhRqgAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
