e5f2a3510697829a3d8571e03d59f2a49d66541e
rachelemoore <124698533+rachelemoore@users.noreply.github.com>
2026-03-21 18:19:09 +0000

    Deploying to gh-pages from @ rachelemoore/rachelemoore.github.io@6310eea0c48498e4b92118e5b2c3296f88603fc2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -525,13 +525,6 @@ ninja.data = [{
           window.open("https://orcid.org/0000-0002-5426-9545", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
