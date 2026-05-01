ed59fcf560ae8e95b2d3bd62640ccd56a08410cc
rachelemoore <124698533+rachelemoore@users.noreply.github.com>
2026-05-01 09:20:44 +0000

    Deploying to gh-pages from @ rachelemoore/rachelemoore.github.io@5c593d7f31b7491ff7780a83ae002bdf4d0aaa08 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,9 +42,9 @@ ninja.data = [{
           description: "Houart Lab, KCL",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-understanding-axon-initiation-in-vivo",
+          title: 'Understanding axon initiation in vivo',
+          description: "Clarke Lab, KCL",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project.html";
             },},{id: "projects-project-4",
