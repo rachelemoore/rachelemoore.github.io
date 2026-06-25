7f1172973682a7dce5e3a6f1605ce750cfca687a
rachelemoore <124698533+rachelemoore@users.noreply.github.com>
2026-06-25 20:08:59 +0000

    Deploying to gh-pages from @ rachelemoore/rachelemoore.github.io@0bc24c85e015314b6597434bc6cbe5565290d97e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,7 +72,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/MooreR_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
